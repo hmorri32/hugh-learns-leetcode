b4378a259ddd9fc32141476819b045bf0e5d67f0
Hugh Morrison <hmorri32@gmail.com>
2023-10-04 16:42:30 -0400

    217 + brute force, sorted search, set take 2

--- 217/containsDuplicate.js
+++ 217/containsDuplicate.js
@@ -1,3 +1,7 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+// Hash Set
+// Time O(N) | Space O(N)
 export default containsDuplicate = (nums) => {
   const set = new Set();
 
@@ -9,7 +13,28 @@ export default containsDuplicate = (nums) => {
   return false;
 };
 
-function take2(nums) {
+const setTake2 = (nums) => {
   const numsSet = new Set(nums);
   return numsSet.size !== nums.length;
-}
+};
+
+// brute force, linear search
+// Time O(N^2) | Space O(1)
+export const bruteForceSearch = (nums) => {
+  for (let right = 0; right < nums.length; right++) {
+    for (let left = 0; left < right; left++) {
+      if (nums[left] === nums[right]) return true;
+    }
+  }
+  return false;
+};
+
+// Time O(N * log(N)) | Space O(1)
+export const sortedSearch = (nums) => {
+  const sorted = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === sorted[i + 1]) return true;
+    return false;
+  }
+};
--- 217/containsDuplicate.test.js
+++ 217/containsDuplicate.test.js
@@ -1,9 +1,28 @@
-import containsDuplicate from './containsDuplicate';
+import containsDuplicate, {
+  sortedSearch,
+  bruteForceSearch,
+} from './containsDuplicate';
 
 describe('containsDuplicate', () => {
   it('should return true if array contains duplicate', () => {
     expect(containsDuplicate([1, 2, 3, 1])).toBe(true);
     expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
     expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true);
+  });
+});
+
+describe('sortedSearch', () => {
+  it('should return true if array contains duplicate', () => {
+    expect(sortedSearch([1, 2, 3, 1])).toBe(true);
+    expect(sortedSearch([1, 2, 3, 4])).toBe(false);
+    expect(sortedSearch([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true);
+  });
+});
+
+describe('bruteForceSearch', () => {
+  it('should return true if array contains duplicate', () => {
+    expect(bruteForceSearch([1, 2, 3, 1])).toBe(true);
+    expect(bruteForceSearch([1, 2, 3, 4])).toBe(false);
+    expect(bruteForceSearch([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true);
   });
 });
